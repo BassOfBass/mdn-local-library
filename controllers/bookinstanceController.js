e18f739d378092184231d1be43c48896c58e025f
Peter Buttgig <vinniethepooloo@protonmail.com>
2020-09-17 00:16:38 +0500

    part 5 complete

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,6 +1,11 @@
 const BookInstance = require('../models/bookinstance');
 
-// Display list of all BookInstances.
+/**
+ * Display list of all BookInstances.
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 exports.bookinstance_list = (req, res, next) => {
 
   BookInstance.find()
@@ -13,9 +18,28 @@ exports.bookinstance_list = (req, res, next) => {
 
 };
 
-// Display detail page for a specific BookInstance.
-exports.bookinstance_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+/**
+ * Display detail page for a specific BookInstance.
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.bookinstance_detail = (req, res, next) => {
+  BookInstance
+  .findById(req.params.id)
+  .populate('book')
+  .exec((err, bookinstance) => {
+
+      if (err) { return next(err); }
+
+      if (bookinstance == null) { // No results.
+        let err = new Error('Book copy not found');
+        err.status = 404;
+        return next(err);
+      }
+
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Copy: ' + bookinstance.book.title, bookinstance: bookinstance });
+  });
 };
 
 // Display BookInstance create form on GET.
