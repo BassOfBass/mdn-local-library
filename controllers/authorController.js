19e73e58b1b30316deab3c05d2d4ddcc40c36264
Peter Buttgig <vinniethepooloo@protonmail.com>
2020-09-16 23:31:58 +0500

    author detail page complete

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,8 +1,14 @@
+const async = require('async');
+const Book = require('../models/book');
 const Author = require('../models/author');
 
-// Display list of all Authors.
+/**
+ * Display list of all Authors.
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 exports.author_list = (req, res, next) => {
-
   Author.find()
   .populate('author')
   .sort([['family_name', 'ascending']])
@@ -11,14 +17,41 @@ exports.author_list = (req, res, next) => {
       //Successful, so render
       res.render('author_list', { title: 'Author List', author_list: list_authors });
     });
-
 };
 
-// Display detail page for a specific Author.
-exports.author_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
-};
+/**
+ * Display detail page for a specific Author.
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+exports.author_detail = (req, res, next) => {
 
+  async.parallel({
+    author: (callback) => {
+      Author
+      .findById(req.params.id)
+      .exec(callback);
+    },
+    authors_books: (callback) => {
+      Book
+      .find({ 'author': req.params.id }, 'title summary')
+      .exec(callback);
+    },
+  }, (err, results) => {
+
+    if (err) { return next(err); } // Error in API usage.
+
+    if (results.author == null) { // No results.
+      let err = new Error('Author not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    // Successful, so render.
+    res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
+  });
+};
 // Display Author create form on GET.
 exports.author_create_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Author create GET');
