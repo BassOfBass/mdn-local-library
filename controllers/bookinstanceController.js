45be0929366f975b76af0c990be052cfc1768d3f
Peter Buttgig <vinniethepooloo@protonmail.com>
2020-09-18 19:25:25 +0500

    book instance form complete

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
+const validator = require("express-validator");
 const BookInstance = require('../models/bookinstance');
+const Book = require('../models/book');
 
 /**
  * Display list of all BookInstances.
@@ -11,7 +13,9 @@ exports.bookinstance_list = (req, res, next) => {
   BookInstance.find()
   .populate('book')
   .exec((err, list_bookinstances) => {
+
     if (err) { return next(err); }
+    
     // Successful, so render
     res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
   });
@@ -24,8 +28,7 @@ exports.bookinstance_list = (req, res, next) => {
  * @param {*} res 
  */
 exports.bookinstance_detail = (req, res, next) => {
-  BookInstance
-  .findById(req.params.id)
+  BookInstance.findById(req.params.id)
   .populate('book')
   .exec((err, bookinstance) => {
 
@@ -42,15 +45,84 @@ exports.bookinstance_detail = (req, res, next) => {
   });
 };
 
-// Display BookInstance create form on GET.
-exports.bookinstance_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance create GET');
+/**
+ * Display BookInstance create form on GET.
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+exports.bookinstance_create_get = (req, res, next) => {
+
+  Book.find({}, 'title')
+  .exec((err, books) => {
+
+    if (err) { return next(err); }
+
+    // Successful, so render.
+    res.render('bookinstance_form', { title: 'Create BookInstance', book_list: books });
+  });
 };
 
-// Handle BookInstance create on POST.
-exports.bookinstance_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+/**
+ * Handle BookInstance create on POST.
+ */
+exports.bookinstance_create_post = [
+
+  // Validate fields.
+  validator.body('book', 'Book must be specified').trim().isLength({ min: 1 }),
+  validator.body('imprint', 'Imprint must be specified').trim().isLength({ min: 1 }),
+  validator.body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601(),
+  
+  // Sanitize fields.
+  validator.sanitizeBody('book').escape(),
+  validator.sanitizeBody('imprint').escape(),
+  validator.sanitizeBody('status').trim().escape(),
+  validator.sanitizeBody('due_back').toDate(),
+  
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+ 
+    /** The validation errors extracted from a request. */
+    const errors = validator.validationResult(req);
+
+    /** A BookInstance object with escaped and trimmed data. */
+    const bookinstance = new BookInstance({ 
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back
+    });
+
+    if ( !errors.isEmpty() ) {
+        // There are errors. Render form again with sanitized values and error messages.
+        Book.find({},'title')
+        .exec((err, books) => {
+
+          if (err) { return next(err); }
+
+          // Successful, so render.
+          res.render('bookinstance_form', { 
+            title: 'Create BookInstance', 
+            book_list: books, 
+            selected_book: bookinstance.book._id, 
+            errors: errors.array(), 
+            bookinstance: bookinstance });
+        });
+
+        return;
+
+    } else {
+      // Data from form is valid.
+      bookinstance.save((err) => {
+
+        if (err) { return next(err); }
+        
+        // Successful - redirect to new record.
+        res.redirect(bookinstance.url);
+      });
+    }
+  }
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = (req, res) => {
